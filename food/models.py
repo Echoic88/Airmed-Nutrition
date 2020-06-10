854e24b8cc3406d67667215175bc0fb1c99b5f04
Echoic88 <52455390+Echoic88@users.noreply.github.com>
2020-06-10 14:21:46 +0100

    Add FoodBase model Food app: models: add food base model with tests
    settings.py
    add food app to installed apps

--- food/models.py
+++ food/models.py
@@ -1,5 +1,9 @@
 import uuid
 from django.db import models
+from django.core.validators import ValidationError
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,4 +12,90 @@ class Brand(models.Model):
     Model for food Brand names
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if self.name is None or self.name == "":
+            raise ValidationError(
+                "Name is required", code="required_field"
+            )
+
+
+class FoodBase(models.Model):
+    """
+    Store base food nutritional information. Nutrional values
+    in this model containf values per 100g/100ml inline with
+    EU packaging requirements.
+    fat_unsaturated and carb_non_sugar fields are calculated
+    using pre-save receiver rather than user entry
+    """
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    name = models.CharField(
+        max_length=70, null=False, blank=False
+    )
+    description = models.TextField(
+        max_length=200, blank=True
+    )
+    user = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL
+    )
+    brand = models.ForeignKey(
+        Brand, null=True, on_delete=models.SET_NULL
+    )
+    energy = models.PositiveSmallIntegerField(
+        null=True
+    )
+    fat_total = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    fat_saturated = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    fat_unsaturated = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    carb_total = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    carb_sugar = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    carb_non_sugar = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    fibre = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    protein = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    salt_amount = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True
+    )
+    tags = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.name} : {self.brand.name}'
+
+    def clean(self):
+        if type(self.energy) is not int:
+            raise ValidationError(
+                "Please enter a whole number", code="non_integer"
+            )
+
+
+@receiver(pre_save, sender=FoodBase)
+def compare_answers(sender, instance, *args, **kwargs):
+    """
+    calculate fat_unsaturated and carb_non_sugars
+    """
+    if instance.fat_total is not None or instance.fat_total != 0:
+        instance.fat_unsaturated = instance.fat_total - instance.fat_saturated
+
+    if instance.carb_total is not None or instance.carb_total != 0:
+        instance.carb_non_sugar = instance.carb_total - instance.carb_sugar
--- food/tests_models.py
+++ food/tests_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from .models import Brand
+from django.core.validators import ValidationError
+from django.contrib.auth.models import User
+from .models import Brand, FoodBase
 
 
 # Create your tests here.
@@ -15,3 +17,91 @@ class TestBrandModel(TestCase):
         retrieve = Brand.objects.get(name="test_brand")
         self.assertIsInstance(retrieve, Brand)
         self.assertEqual(retrieve.name, test.name)
+
+    def test_brand_is_required(self):
+        test = Brand(name="")
+        with self.assertRaises(ValidationError):
+            test.clean()
+
+
+class TestFoodBaseModel(TestCase):
+    """
+    Tests FoodBase model
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test_user",
+            password="1290Pass"
+        )
+
+        self.brand = Brand.objects.create(name="test_brand")
+
+        self.food_data = {
+            "name": "test_food",
+            "user": self.user,
+            "brand": self.brand,
+            "energy": 50,
+            "fat_total": 3.6,
+            "fat_saturated": 1.1,
+            "carb_total": 2.4,
+            "carb_sugar": 0.6,
+            "fibre": 1.5,
+            "protein": 1.5,
+            "salt_amount": 1.5,
+        }
+
+    def test_saves_with_expected_valid_data(self):
+        """
+        test all fields except tags
+        """
+        FoodBase.objects.create(**self.food_data)
+        retrieve = FoodBase.objects.get(name="test_food")
+        self.assertIsInstance(retrieve, FoodBase)
+        self.assertEqual(retrieve.name, self.food_data["name"])
+        self.assertEqual(retrieve.user, self.food_data["user"])
+        self.assertIsInstance(retrieve.user, User)
+        self.assertEqual(retrieve.brand, self.food_data["brand"])
+        self.assertIsInstance(retrieve.brand, Brand)
+        self.assertEqual(retrieve.energy, self.food_data["energy"])
+        self.assertEqual(
+            float(retrieve.fat_total), self.food_data["fat_total"]
+        )
+        self.assertEqual(
+            float(retrieve.fat_saturated), self.food_data["fat_saturated"]
+        )
+        self.assertEqual(
+            float(retrieve.carb_total), self.food_data["carb_total"]
+        )
+        self.assertEqual(
+            float(retrieve.carb_sugar), self.food_data["carb_sugar"]
+        )
+        self.assertEqual(
+            float(retrieve.fibre), self.food_data["fibre"]
+        )
+        self.assertEqual(
+            float(retrieve.protein), self.food_data["protein"]
+        )
+        self.assertEqual(
+            float(retrieve.salt_amount), self.food_data["salt_amount"]
+        )
+
+    def test_calculated_fields_are_saved_correctly(self):
+        """
+        fat_unsaturated and carb_non_sugars are calculated
+        using pre-save receiver
+        """
+        food = FoodBase.objects.create(**self.food_data)
+
+        self.assertEqual(
+            food.fat_unsaturated, food.fat_total-food.fat_saturated
+        )
+        self.assertEqual(
+            food.carb_non_sugar, food.carb_total-food.carb_sugar
+        )
+
+    def test_raise_validation_error_if_energy_field_is_decimal(self):
+        self.food_data["energy"] = 1.5
+        food = FoodBase(self.food_data)
+        with self.assertRaisesMessage(
+                ValidationError, "Please enter a whole number"):
+            food.clean()
